c97800cbae449109be300af0b0f5a819d1bb29e2
jysh1214 <jyxemperor@gmail.com>
2019-02-19 14:50:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # imgur key
-client_id = 'a2cd23e7ae19690'
-client_secret = '467f239fcc53f35cb702d6a56ba8ff0d0b99f743'
-album_id = 'rlslDfL'
-access_token = '6f9df0f3abf5ab2c256530a5cee575c4070f2fce'
-refresh_token = '725a84e9d8df74dab19be03ea9a0710287905dff'
+client_id = 
+client_secret = 
+album_id = 
+access_token = 
+refresh_token = 
